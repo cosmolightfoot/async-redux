0b88d6888b5d4347a7653608e77e57d3ec709a99
Cosmo Kent <cosmolightfoot@gmail.com>
2019-05-30 10:12:32 -0700

    created Character component

--- src/components/Character.js
+++ src/components/Character.js
@@ -2,7 +2,12 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 function Character({ character }) {
-  
+  return (
+    <>
+      <img src={character.imageUrl} />
+      <h3>{character.name}</h3>
+    </>
+  );
 }
 
 Character.propTypes = {
