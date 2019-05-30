1c7ec53910c5544b41df0a890baedcf6ba0c787e
Cosmo Kent <cosmolightfoot@gmail.com>
2019-05-30 10:34:06 -0700

    created fetch characters action

--- src/components/Characters.js
+++ src/components/Characters.js
@@ -1,8 +1,19 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import Character from './Character';
 
 function Characters({ characters }) {
-
+  const charactersList = characters.map(character => (
+    <li key={character.id}>
+      <Character character={character} />
+    </li>
+  ));
+  
+  return (
+    <ul>
+      {charactersList}
+    </ul>
+  );
 }
 
 Characters.propTypes = {
